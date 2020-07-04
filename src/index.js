bcf1585e0d1e67b68e3b89e9ca056cbce58eeaad
JYPs <qkrwndma5@naver.com>
2020-07-04 17:50:23 +0900

    Provider 추가(Provider는 어플리케이션을 store에 연결하는 역할)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { Provider } from "react-redux";
+import store from "./store";
 
-ReactDOM.render(<App></App>, document.getElementById("root"));
+/* 
+  The <Provider /> makes the Redux store available to any nested components that have been wrapped in the connect() function.
+  인덱스에서 내 어플리케이션을 store에 연결함
+  ==> 리덕스 저장소를 사용할 수 있게 함
+*/
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
